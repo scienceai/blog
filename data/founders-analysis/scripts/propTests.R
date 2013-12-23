be2572e39d7c16873f7250584de2042aac60361b
Sebastien Ballesteros <sebastien.ballesteros@gmail.com>
2013-12-22 20:08:57 -0500

    rename data package

--- data/founders-analysis/scripts/propTests.R
+++ data/founders-analysis/scripts/propTests.R
@@ -1,4 +1,4 @@
-schools <- read.csv("data_modules/unicorns/data/schools.csv")
+schools <- read.csv("../data_modules/unicorns/data/schools.csv")
 stanford <- schools$Unicorns[schools$Schools=="Stanford"]
 harvard <- schools$Unicorns[schools$Schools=="Harvard"]
 prop.test(stanford ,stanford + harvard, alternative="greater")
