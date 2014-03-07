32ccecebdb66d1e23f410ab7baa73648ac945f61
JDureau <dureau.joseph@gmail.com>
2014-03-07 20:21:50 +0100

    updated examples

--- data/founders-analysis/scripts/propTests.R
+++
@@ -1,4 +0,0 @@
-schools <- read.csv("../datapackages/founders/data/schools.csv")
-stanford <- schools$unicorns[schools$school=="Stanford"]
-harvard <- schools$unicorns[schools$school=="Harvard"]
-prop.test(stanford ,stanford + harvard, alternative="greater")
